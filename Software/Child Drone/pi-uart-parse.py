82189295dc2255eed137b2dc97041a21bb8eaf6b
Kyle Douglas <kadouglas@umail.ucsb.edu>
2019-03-15 23:49:02 +0000

    better uart support

--- Software/Child Drone/pi-uart-parse.py
+++ Software/Child Drone/pi-uart-parse.py
@@ -9,13 +9,13 @@ ser = serial.Serial('/dev/ttyS0',baudrate=9600,timeout=0.5)
 while(1):
     ser.flushInput();
     #Parse Data
-    line = self.ser.readline()
+    line = ser.readline()
     words = string.split(line,",")
     if len(words) == 8 and words[0] == 'X' and words[2] == 'Y' and words[4] == 'Z' and words[6] == 'R':
-        x = int(words[1])
-        y = int(words[3])
-        z = int(words[5])
-        r = int(words[7])
+        x = float(words[1])
+        y = float(words[3])
+        z = float(words[5])
+        r = float(words[7])
 
     else:
         print(words);
--- Software/Child Drone/test-uart.py
+++ Software/Child Drone/test-uart.py
@@ -4,11 +4,9 @@ import string
 import time
 
 #Initialize Serial Object
-ser = serial.Serial('/dev/ttyS0',baudrate=9600,timeout=0.5)
+ser = serial.Serial('/dev/ttyS0',baudrate=9600,timeout=10)
 
 while(1):
-    ser.flushInput();
     #Parse Data
-    line = self.ser.readline()
+    line = ser.readline()
     print(line)
-    time.sleep(100)
