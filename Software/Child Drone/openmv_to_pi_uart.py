cc67fb1487b5f14550fe7a2ec06e3e39970b1777
kadouglas <kadouglas@umail.ucsb.edu>
2019-05-15 13:16:29 -0700

    added rotation adjustment

--- Software/Child Drone/openmv_to_pi_uart.py
+++ Software/Child Drone/openmv_to_pi_uart.py
@@ -20,7 +20,18 @@ while(True):
     clock.tick()
     img = sensor.snapshot()
     for tag in img.find_apriltags():
-        print_args = (tag.x_translation(), tag.y_translation(), tag.z_translation(),
-        degrees(tag.z_rotation()))
+        x_tag = tag.x_translation()
+        y_tag = tag.y_translation()
+        z_tag = tag.z_translation()
+        
+        x_rot = tag.x_rotation()
+        y_rot = tag.y_rotation()
+        z_rot = tag.z_rotation()
+        
+        #Trig
+        x = math.cos(y_rot)*x_tag - math.sin(y_rot)*z_tag
+        y = math.sin(x_rot)*math.sin(y_rot)*x_tag + math.cos(x_rot)*y_tag + math.cos(y_rot)*math.sin(x_rot)*z_tag
+        z = math.cos(x_rot)*math.sin(y_rot)*x_tag - math.sin(x_rot)*y_tag + math.cos(x_rot)*math.cos(y_rot)*z_tag
+        
+        print_args = (x, y, z, z_rot)
         uart.write("X,%f,Y,%f,Z,%f,R,%f\r\n" % print_args)
-
